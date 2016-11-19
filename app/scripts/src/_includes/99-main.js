67caf12ba2b187ad56353cb7e45c7e096eb0f894
Rafael Birkmann <mail@rafaelbirkmann.de>
2016-11-19 12:27:27 +0100

    del

--- app/scripts/src/_includes/99-main.js
+++ app/scripts/src/_includes/99-main.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-
-	$('input, textarea').placeholder();
-
-	$(".nav-toggle").click(function(){
-		$("body").toggleClass("open");
-		$(".header-main .right").slideToggle(300);
-	});
-
+	
 });
